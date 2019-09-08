b0a9ca100c5568c3f7c35039942153fbc347880c
ogabriel <gabrieloliver8991@gmail.com>
2019-09-08 15:06:01 -0300

    add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,48 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Theater.Entertainments
+
+movies_data = [
+  %{
+    title: "Apocalypto",
+    summary: """
+    As the Mayan kingdom faces its decline,
+    a young man is taken on a perilous journey
+    to a world ruled by fear and oppression.
+    """,
+    year: 2006
+  },
+  %{
+    title: "Black Swan",
+    summary: """
+    A committed dancer struggles to maintain her
+    sanity after winning the lead role in a production
+    of Tchaikovsky's "Swan Lake".
+    """,
+    year: 2010
+  },
+  %{
+    title: "Braveheart",
+    summary: """
+    When his secret bride is executed for assaulting
+    an English soldier who tried to rape her, William
+    Wallace begins a revolt against King Edward I of England.
+    """,
+    year: 1995
+  },
+  %{
+    title: "Drive",
+    summary: """
+    A mysterious Hollywood stuntman and mechanic moonlights
+    as a getaway driver and finds himself in trouble when
+    he helps out his neighbor.
+    """,
+    year: 2011
+  }
+]
+
+Enum.each(movies_data, fn(data) ->
+  Entertainments.create_movie(data)
+end)
